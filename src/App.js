179d309cd980067b44ecd43096df51ac1614834e
yh9035926 <yh9035926@naver.com>
2022-08-30 22:17:57 +0900

    Effect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Callback from "./components/Callback";
+import Effect from "./components/Effect";
 import Memo from "./components/Memo";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <div>안녕하세요</div>
       <Memo />
       <Callback></Callback>
+      <Effect></Effect>
     </div>
   );
 };
